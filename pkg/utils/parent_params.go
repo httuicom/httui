2d3e2f27162c9bd3986b751db12b7be685072a19
gandarfh <jgdsantosferreira@gmail.com>
2025-07-22 17:30:07 -0300

    refactor: introduce deep copy utility for safer mutations

--- pkg/utils/parent_params.go
+++ pkg/utils/parent_params.go
@@ -2,24 +2,38 @@ package utils
 
 import "github.com/gandarfh/httui/internal/storage"
 
+func CopySliceOfMap[K comparable, V any](src []map[K]V) []map[K]V {
+	copied := make([]map[K]V, len(src))
+	for i, m := range src {
+		newMap := make(map[K]V)
+		for k, v := range m {
+			newMap[k] = v
+		}
+		copied[i] = newMap
+	}
+	return copied
+}
+
 func GetAllParentsHeaders(parentId *uint, headers []map[string]string) []map[string]string {
+	updatedHeaders := CopySliceOfMap(headers)
+
 	if parentId != nil {
 		parent, _ := storage.NewRequest().FindOne(*parentId)
 		parentHeaders := GetAllParentsHeaders(parent.ParentID, parent.Headers.Data())
-
-		headers = append(headers, parentHeaders...)
+		updatedHeaders = append(updatedHeaders, CopySliceOfMap(parentHeaders)...)
 	}
 
-	return headers
+	return updatedHeaders
 }
 
 func GetAllParentsParams(parentId *uint, params []map[string]string) []map[string]string {
+	updatedParams := CopySliceOfMap(params)
+
 	if parentId != nil {
 		parent, _ := storage.NewRequest().FindOne(*parentId)
 		parentParams := GetAllParentsParams(parent.ParentID, parent.QueryParams.Data())
-
-		params = append(params, parentParams...)
+		updatedParams = append(updatedParams, CopySliceOfMap(parentParams)...)
 	}
 
-	return params
+	return updatedParams
 }
--- pkg/utils/process_operator.go
+++ pkg/utils/process_operator.go
@@ -1,11 +1,13 @@
 package utils
 
 func ProcessParamsOperators(values []map[string]string, workspaceId uint, envs map[string]string) []map[string]string {
+	updatedEnvs := CopySliceOfMap(values)
+
 	for i, item := range values {
 		for k, v := range item {
-			values[i][k] = ReplaceByOperator(v, workspaceId, envs)
+			updatedEnvs[i][k] = ReplaceByOperator(v, workspaceId, envs)
 		}
 	}
 
-	return values
+	return updatedEnvs
 }
